f2e80ee0e6fad2c504d4722827b22ea75edad82c
tpeea <adufosse43@live.fr>
2019-06-11 12:17:52 +0200

    test

--- app/src/main/java/com/example/slider/BTConnectActivity.java
+++ app/src/main/java/com/example/slider/BTConnectActivity.java
@@ -2,12 +2,17 @@ package com.example.slider;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class BTConnectActivity extends AppCompatActivity {
+
+    Toolbar toolbar_osci;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_btconnectactivity);
+        toolbar_osci = findViewById(R.id.toolbar);
+        setSupportActionBar(R.id.toolbar);
     }
 }
